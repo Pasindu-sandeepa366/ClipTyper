3d5a63e70e2e12527aeb1c5206adcc9780a57134
Pasindu Sandeepa <59728642+Pasindu-sandeepa366@users.noreply.github.com>
2021-08-11 12:50:43 +0530

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,4 @@
+echo Install requirements
+pip install -r requirements.txt
+echo 'Opening github page for more details and screenshots' 
+open https://github.com/Pasindu-sandeepa366/ClipTyper
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo Install requirements
+pip install -r requirements.txt
+echo 'Opening github page for more details and screenshots' 
+open https://github.com/Pasindu-sandeepa366/ClipTyper
